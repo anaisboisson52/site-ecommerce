04b4beff3aade4992a7b88cb3f38fcc43ec4bef3
Anais Boisson <anaisboisson52@gmail.com>
2025-06-12 14:14:24 +0200

    initialisation

---
+++ src/main/java/com/epsi/site_ecommerce/SiteEcommerceApplication.java
@@ -0,0 +1,13 @@
+package com.epsi.site_ecommerce;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SiteEcommerceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SiteEcommerceApplication.class, args);
+	}
+
+}
